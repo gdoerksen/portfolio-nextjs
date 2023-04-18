39790a3e41ad087563ad0d1f0f83e840b10f96d3
Graham Doerksen <46031209+gdoerksen@users.noreply.github.com>
2023-04-18 09:56:43 -0600

    Different logo colors

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,9 +11,8 @@ const Logo = () => {
             className='w-16 h-16 bg-dark text-light flex items-center justify-center
             rounded-full text-2xl font-bold'
             whileHover={{
-                backgroundColor:['#121212', "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
+                backgroundColor:["#0A4D68", "#088395", "#05BFDB", "#00FFCA", "#05BFDB", "#088395", "#0A4D68"],
                 transition:{duration:1, repeat: Infinity},
-                // scale:1.5
             }}
             >
             GD
