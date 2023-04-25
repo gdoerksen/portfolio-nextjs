adaf785a247392c0e1771976898d3ac4a9c4602d
Graham Doerksen <46031209+gdoerksen@users.noreply.github.com>
2023-04-25 08:41:52 -0600

    Improved reactivity on contact button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,32 @@ import Link from 'next/link';
 import { useInView } from 'react-intersection-observer';
 import { FaComments, FaFileDownload } from 'react-icons/fa';
 
-
-
 import profilePic from '../../public/images/profile/profile.jpg';
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home: NextPage = () => {
+
+  return (
+    <Layout className='pt-0'>
+      <div className='flex justify-center'>
+        <div className="block md:flex max-w-screen-lg items-center ">
+          {/* TODO: Consider use of max-w-prose on this container */}
+          <div className='md:w-1/2' >
+            <Image src={profilePic} alt="Picture of the author" className='object-contain h-[30rem]' />
+          </div>
+
+          <IntroText />
+
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
+
+let IntroText = () => {
 
   const { ref, inView } = useInView({
     triggerOnce: true,
@@ -21,43 +40,31 @@ const Home: NextPage = () => {
   });
 
   let button_styling = `bg-dark text-light dark:bg-light dark:text-dark 
-    py-3 px-6 rounded-lg text-lg font-semibold 
-    transition-all hover:bg-primary dark:hover:bg-primary dark:hover:text-light
-    outline-none ring-primary 
-    focus-visible:ring-2 focus-visible:bg-light focus-visible:text-dark focus-visible:animate-bounce` // Refactor This
+  py-3 px-6 rounded-lg text-lg font-semibold 
+  transition-all hover:bg-primary dark:hover:bg-primary dark:hover:text-light
+  outline-none ring-primary 
+  focus-visible:ring-2 focus-visible:bg-light focus-visible:text-dark focus-visible:animate-bounce` // Refactor This
 
   return (
-    <Layout className='pt-0'>
-        <div className='flex justify-center'>
-          <div className="block md:flex max-w-screen-lg items-center ">
-            {/* TODO: Consider use of max-w-prose on this container */}
-            <div className='md:w-1/2' >
-              <Image src={profilePic} alt="Picture of the author" className='object-contain h-[30rem]' />
-            </div>
-            
-            <div ref={ref} className={`p-4 md:w-1/2 duration-1000 ${inView ? 'opacity-1' : 'opacity-0 translate-x-16'}`}>
-              <h1 className='sm:text-4xl text-2xl font-bold'>Hi, I&apos;m <span className='text-primary dark:text-dark_primary'>Graham </span></h1>
-              {/* <AnimatedText text="This is a test" className='h-10 bg-primary text-dark'/> */}
-              <br />
-              
-              <h2 className='sm:text-2xl text-lg font-bold'>I&apos;m a <span className='text-primary dark:text-dark_primary'>Software Developer</span></h2>
-              <br />
-              
-              <div className='flex items-center self-start mt-2'>
-                <Link href='/docs/resumes/Resume_Base.pdf' locale={false} target={"_blank"} download="Graham-Doerksen-Resume"
-                  className={button_styling}>
-                  Resume <FaFileDownload className='inline-flex' />
-                </Link>
-                <Link href='/contact' target={"_blank"} className={"ml-4 text-lg font-semibold underline focus-visible:animate-bounce"}>
-                  Contact <FaComments className='inline-flex' />
-                </Link>
-              </div>
-            </div>
+    <div ref={ref} className={`p-4 md:w-1/2 duration-1000 ${inView ? 'opacity-1' : 'opacity-0 translate-x-12'}`}>
+      <h1 className='sm:text-4xl text-2xl font-bold'>Hi, I&apos;m <span className='text-primary dark:text-dark_primary'>Graham </span></h1>
+      {/* <AnimatedText text="This is a test" className='h-10 bg-primary text-dark'/> */}
+      <br />
 
-          </div>
-        </div>
-          </Layout>
-  );
-};
+      <h2 className='sm:text-2xl text-lg font-bold'>I&apos;m a <span className='text-primary dark:text-dark_primary'>Software Developer</span></h2>
+      <br />
 
-export default Home;
+      <div className='flex items-center self-start mt-2'>
+        <Link href='/docs/resumes/Resume_Base.pdf' locale={false} target={"_blank"} download="Graham-Doerksen-Resume"
+          className={button_styling}>
+          Resume <FaFileDownload className='inline-flex' />
+        </Link>
+        <Link href='/contact' target={"_blank"} 
+          className={"ml-4 text-lg font-semibold underline py-3 px-6 transition-all rounded-lg ring-dark hover:ring-primary ring-2  focus-visible:animate-bounce"}>
+          Contact <FaComments className='inline-flex' />
+        </Link>
+      </div>
+    </div>
+  )
+
+}
